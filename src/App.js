22ec713533289f65f8c422216cb6329601ae3b99
Justin Foster <jfoster91@gmail.com>
2020-07-31 23:43:56 -0400

    New and deleted files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import STORE from './store.js';
+import List from './List';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    Store: STORE
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Trelloyes!</h1>
+        
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/List.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Card from './Card';
+
+function List(props) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default List;
